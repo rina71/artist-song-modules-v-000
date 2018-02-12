b79fc8c8ebc8e29f49c6fcd6c910bce1a7e5463f
Yurina Shin <yurinas@gmail.com>
2018-02-12 05:22:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,8 +3,8 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable ::ClassMethods
-  include Memorable :: InstanceMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
 
